faf9cda00212b150e5849ccfffed76b5dacf7087
Bruce McClure <brucemcc90@gmail.com>
2019-11-18 16:34:21 +1100

    Basic Express Server and Heroku deployment steps

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');         // common js import style
+const app = express();                      // new app running express
+
+// Example of a route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there'});
+});
+
+
+// This is done so Heroku can inject the correct port into our app. or 5000 (development)
+const PORT = process.env.port || 5000;
+app.listen(PORT);
